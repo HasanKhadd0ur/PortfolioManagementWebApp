4364b265fbbcd119173d85ef8ff0b4c01b565170
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-04-23 16:17:32 +0300

    some update on entities , and db context , connection string

---
+++ cleanArchWebApp.Infrastructure/ProtofoiloDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace cleanArchWebApp.Infrastructure
+{
+    class ProtofoiloDBContext : DbContext
+    {
+        public ProtofoiloDBContext()
+        {
+        }
+
+        public ProtofoiloDBContext(DbContextOptions<ProtofoiloDBContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Owner> MyProperty { get; set; }
+
+
+    }
+}
