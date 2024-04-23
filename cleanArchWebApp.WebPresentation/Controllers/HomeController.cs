d6ed8b23085a1d630f4099211f07e9a514c0a023
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-04-23 09:54:07 +0300

    update index  & about   views update layout

--- cleanArchWebApp.WebPresentation/Controllers/HomeController.cs
+++ cleanArchWebApp.WebPresentation/Controllers/HomeController.cs
@@ -12,5 +12,10 @@ namespace cleanArchWebApp.WebPresentation.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
