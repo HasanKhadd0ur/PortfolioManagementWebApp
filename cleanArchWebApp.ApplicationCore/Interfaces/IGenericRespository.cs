804cb7e5e7ba8a9df1bc6878f9c4503c350de10f
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-04-29 20:02:06 +0300

    adding a gerneric repository and unit of work

---
+++ cleanArchWebApp.ApplicationCore/Interfaces/IGenericRespository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cleanArchWebApp.ApplicationCore.Interfaces
+{
+    interface IGenericRespository<T> where T : class 
+    {
+        IEnumerable<T> GetAll();
+        T Update(T entities);
+        T Insert(T entities);
+        T GetById(object  id);
+        void Delete(object id);
+    }
+}
---
+++ cleanArchWebApp.ApplicationCore/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cleanArchWebApp.ApplicationCore.Interfaces
+{
+    interface IUnitOfWork<T> where T : class 
+    {
+        IGenericRespository<T> Entity { get; set; }
+        void Save(); 
+
+    }
+}
