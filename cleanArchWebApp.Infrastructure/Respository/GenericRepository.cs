52b2ed1f5d593a4ed79f07d91ca05e8d1f92551b
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-04-29 20:04:21 +0300

    adding generic repository

---
+++ cleanArchWebApp.Infrastructure/Respository/GenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using cleanArchWebApp.ApplicationCore.
+namespace cleanArchWebApp.Infrastructure.Respository
+{
+    class GenericRepository
+    {
+    }
+}
