76ffb96bc8b6feea6436f839c73c22ac6416a1b0
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-04-29 19:44:56 +0300

    update views

--- cleanArchWebApp.WebPresentation/Startup.cs
+++ cleanArchWebApp.WebPresentation/Startup.cs
@@ -1,6 +1,8 @@
+using cleanArchWebApp.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +25,11 @@ namespace cleanArchWebApp.WebPresentation
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddDbContext<ProtofoiloDBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
