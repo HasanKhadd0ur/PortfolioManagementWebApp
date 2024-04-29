38c2556741de1b656fe29c74daaf27ee56e3d98a
hasan khaddour <hasan.b.khaddour@gmail.com>
2024-04-29 20:44:51 +0300

    update som interfaces

--- cleanArchWebApp.Infrastructure/Respository/GenericRepository.cs
+++ cleanArchWebApp.Infrastructure/Respository/GenericRepository.cs
@@ -3,11 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using cleanArchWebApp.ApplicationCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
-using cleanArchWebApp.ApplicationCore.
 namespace cleanArchWebApp.Infrastructure.Respository
 {
-    class GenericRepository
+    public class GenericRepository<T> : IGenericRespository<T> where T : class
     {
+        private readonly DbContext _context;
+        private DbSet<T> _table;
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _table   = _context.Set<T>();
+        }
+        void IGenericRespository<T>.Delete(object id)
+        {
+            T entity = _table.Find(id);
+            _table.Remove(entity);
+        }
+
+        IEnumerable<T> IGenericRespository<T>.GetAll()
+        {
+            return _table.ToList();
+        }
+
+        T IGenericRespository<T>.GetById(object id)
+        {
+            return _table.Find(id);
+        }
+
+        T IGenericRespository<T>.Insert(T entity)
+        {
+            return _table.Add(entity).Entity;
+        }
+
+        void  IGenericRespository<T>.Update(T entity)
+        {
+            _table.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+
+        }
     }
 }
---
+++ cleanArchWebApp.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using cleanArchWebApp.ApplicationCore.Interfaces;
+using cleanArchWebApp.Infrastructure.Respository;
+using Microsoft.EntityFrameworkCore;
+
+namespace cleanArchWebApp.Infrastructure.UnitOfWork
+{
+    class UnitOfWork<T> : IUnitOfWork<T> where T : class
+    {
+        private readonly DbContext _context;
+        private IGenericRespository<T> _entity;
+        public UnitOfWork(DbContext context)
+        {
+            _context = context;
+
+        }
+
+        public IGenericRespository<T> Entity
+        {
+                get {
+
+                    return _entity ?? (_entity = new GenericRepository<T>(_context));
+                }
+            
+                
+                
+                
+        }
+
+        void IUnitOfWork<T>.Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
